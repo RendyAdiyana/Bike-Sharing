cb92c7e0423856ba0554b4f12ff499f971d812a8
RendyAdiyana <rendyadiyanabudiman3@gmail.com>
2023-12-21 00:15:38 +0700

    Merubah Dashboard

--- Dashboard/Dashboard_bike_sharing.py
+++ Dashboard/Dashboard_bike_sharing.py
@@ -208,10 +208,10 @@ fig = px.line(hourly_users_df,
 st.plotly_chart(fig, use_container_width=True)
 
 fig3 = px.scatter(df_day, x='temp', y='count', color='season', title='Clusters of bikeshare rides count by season and temperature')
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig3, use_container_width=True)
 
 fig4 = px.scatter(df_day, x='humidity', y='count', color='season', title='Clusters of bikeshare rides count by season and humidity')
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig4, use_container_width=True)
 
 left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig3, use_container_width=True)
